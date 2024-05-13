ce84f75df13351298d8884b5af89f7bb2ad116d9
David Bombara <46980996+dbombara@users.noreply.github.com>
2024-05-13 15:08:50 -0400

    update

--- datadownload.py
+++ datadownload.py
@@ -3,3 +3,4 @@ import pandas as pd
 tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
 data = yf.download(tickers.Symbol.to_list(),'2010-1-1','2022-12-31', auto_adjust=True)['Close']
 data.to_csv('resources/out.csv',index='False')
+
